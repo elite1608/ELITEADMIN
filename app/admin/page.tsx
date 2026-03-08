60c00265ff3cbc621fc1d2f524bd49687b02ed43
elitegymnasticsacademy1608-maker <elitegymnasticsacademy1608@gmail.com>
2026-03-07 20:36:53 -0500

    prueba

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
-import { supabase } from "./lib/supabase";
+import { supabase } from "../lib/supabase";
 
 // --- UTILIDADES DE TIEMPO (BARRANQUILLA) ---
 const diasSemana = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
